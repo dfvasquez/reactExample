b26d88d84ba08ef6845aa2bac6c918aa9d1ab74d
dfvasquez <dfvasquez@uc.cl>
2020-04-25 21:12:08 -0400

    just commited

--- UcApp/src/App.js
+++ UcApp/src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './assets/css/App.css';
 import Header from './views/layout/Header.jsx';
 import Footer from './views/layout/Footer.jsx';
 import Characters from './views/characters/containers/Characters.jsx'
 
+export default function App() {
 
-class App extends React.Component {
 
-  render(){
   return (
-    <div className="App">
-      <Header className='uc-header'/>
-      <div className='container2'>
-        <Characters/>
-      </div>
+    <Router>
+      <div>
       <Footer className="uc-footer"/>
-    </div>
+      <Switch>
+            <Route exact path="/">
+            <div className="App">
+              <Header className='uc-header'/>
+              <div className='container2'>
+                <Characters/>
+              </div>
+              <Footer className="uc-footer"/>
+            </div>
+            </Route>
+            <Route path="/header">
+              <Header className='uc-header'/>
+            </Route>
+            <Route path="/footer">
+              <Footer className="uc-footer"/>
+            </Route>
+      </Switch>
+      </div>
+      
+
+    </Router>
+    
   );
 }
-}
-
-export default App;
